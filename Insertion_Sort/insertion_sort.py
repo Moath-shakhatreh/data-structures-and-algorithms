64a7cf566467a92e965ae905a84f0f5e540e935e
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-07-12 15:01:20 +0000

    I added the test file

--- Insertion_Sort/insertion_sort.py
+++ Insertion_Sort/insertion_sort.py
@@ -2,15 +2,19 @@ def Insert(sorted_list, value):
     i = 0
     while value > sorted_list[i]:
         i = i + 1
-    while i < len(sorted_list):
-        temp = sorted_list[i]
-        sorted_list[i] = value
-        value = temp
-        i = i + 1
-    sorted_list.append(value)
+        if i == len(sorted_list):  # If we reach the end of the list
+            break
+    sorted_list.insert(i, value)  # Use insert() instead of assignment
 
 def InsertionSort(input_list):
+    if len(input_list) == 0:
+        return []  # Return an empty list if input is empty
+
     sorted_list = [input_list[0]]
     for i in range(1, len(input_list)):
         Insert(sorted_list, input_list[i])
     return sorted_list
+
+
+if __name__ == '__main__' :
+    print(InsertionSort([5,12,7,5,5,7]))
